19edebafc8b9fda59e802f20037b297d8486b122
Blessing Awak <melissasignature1@gmail.com>
2023-11-17 20:50:19 +0100

    commit

--- script.js
+++ script.js
@@ -9,10 +9,9 @@ darkBtn.onclick = function () {
   document.body.classList.toggle("dark-theme");
 
   if (localStorage.getItem("theme") == "light") {
-   localStorage.setItem("theme", "dark");
-  }
-  else{
-   localStorage.setItem("theme", "light");
+    localStorage.setItem("theme", "dark");
+  } else {
+    localStorage.setItem("theme", "light");
   }
 };
 
